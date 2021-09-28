916a3a7dcc22e3f267a949da5be09b9d64d8e68e
Giacomo Bonaccorsi <gbonaccorsi@eng.ucsd.edu>
2021-09-28 12:45:22 -0700

    Removed lib.rs from package

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-use shiba::run_until_closed;
-use show_image::create_window;
+use show_image::event::{ModifiersState, VirtualKeyCode, WindowEvent};
+use show_image::{create_window, WindowProxy};
+
+fn run_until_closed(window: WindowProxy) {
+    // Wait for the window to be closed or Escape to be pressed.
+    for event in window.event_channel().unwrap() {
+        if let WindowEvent::KeyboardInput(event) = event {
+            if event.input.key_code == Some(VirtualKeyCode::Escape) {
+                break;
+            } else if event.input.key_code == Some(VirtualKeyCode::C)
+                && event.input.modifiers == ModifiersState::CTRL
+            {
+                break;
+            }
+        }
+    }
+}
 
 const API_URL: &str = "http://shibe.online/api/shibes";
 
